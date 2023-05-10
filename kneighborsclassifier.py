e7e60361b847a3c34df5ae210bb280560a01bf9a
Maciej Sikorski <maciejosikorski@gmail.com>
2023-05-10 21:23:46 +0200

    Distance works properly, I suppose
    Signed-off-by: Maciej Sikorski <maciejosikorski@gmail.com>

--- kneighborsclassifier.py
+++ kneighborsclassifier.py
@@ -18,35 +18,37 @@ class KNeighborsClassifier:
         self.train_set_x = x_train
         self.train_set_y = y_train
 
-    def predict(self, x_test : pd.DataFrame):
-        y_test = np.zeros(len(x_test.index))
-        for x in x_test:
-            X_with_distance = self.train_set_x.copy()
-            X_with_distance["_distance"] = [self.get_distance(i, x) for i in X_with_distance]
-            X_with_distance.sort_values("_distance", inplace=True)
-            X_with_distance = X_with_distance[:self.n_neighbors].value_counts(X_with_distance.iloc[:,-1])
+    def predict(self, x_test : pd.DataFrame) -> np.ndarray:
+        pass
 
+    def get_nearest(self, x_test_item):
+        distances = [self.get_distance(x_test_item, x_train_item, metric=self.metric) for _,x_train_item in self.train_set_x.iterrows()]
+        indices = np.argpartition(distances, self.n_neighbors)[:self.n_neighbors]
+        print(distances)
+        print(indices)
+        
     def calculate_acuracy(x_test, y_test):
         pass
 
     # return distance defined by euclidean metric (i.e. square difference)
-    def euclidean_comparison(self, score_a, score_b):
+    def euclidean_comparison(self, score_a, score_b) -> float:
         return abs(score_a**2 - score_b**2)
     
     # return distance defined by manhattan metric 
-    def manhattan_comparison(self, score_a, score_b):
+    def manhattan_comparison(self, score_a, score_b) -> float:
         return abs(score_a - score_b)
     
     #get distance from A to B defined by metric
-    def get_distance(self, point_a, point_b, metric):
+    def get_distance(self, point_a, point_b, metric) -> float:
         if metric not in self.AVAILABLE_METRICS:
             raise Exception('Metric {} is unknown'.format(metric))
 
         result = 0.0
-        for feature_a, feature_b in zip(point_a, point_b):
+
+        for feature in point_a.index:
             if metric == 'euclidean':
-                result += self.euclidean_comparison(feature_a, feature_b)
+                result += self.euclidean_comparison(point_a[feature], point_b[feature])
             elif metric == 'manhattan':
-                result += self.manhattan_comparison(feature_a, feature_b)
+                result += self.manhattan_comparison(point_a[feature], point_b[feature])
             
         return result
